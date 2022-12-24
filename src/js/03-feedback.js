07544e99201bf3e3a0a1a06f6f77cf1e38375852
NataBelous <natabelous@icloud.com>
2022-12-24 22:09:17 +0100

    fix/08/3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,16 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+  form: document.querySelector(".feedback-form"),
+  input: document.querySelector(".feedback-form textarea"),
+  lable: document.querySelector(".feedback-form lable"),
+};
+refs.form.addEventListener('submit', onformSubmit);
+refs.input.addEventListener('input', throttle(onTextareaInput, 1000));
+
+refs.form.addEventListener('input', evt => {
+  formData[evt.target.name] = evt.target.value;
+  formData.localStorage.setItem(LOCALE_STORAGE_KEY);
+  console.log(formData);
+});
+populateTextarea();
